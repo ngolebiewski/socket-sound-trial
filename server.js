2b1360379337afae9c8f15d00c09aa4754758239
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-07-18 23:18:25 -0400

    add production path

--- server.js
+++ server.js
@@ -14,19 +14,24 @@ const PORT = process.env.PORT || 3000;
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173']
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173'
   },
   connectionStateRecovery: {}
 });
 
-io.listen(5173)
+if (process.env.NODE_ENV !== 'production') {
+  io.listen(5173)
+}
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// Serve static file on root
-// app.get('/', (req, res) => {
-//   res.sendFile(join(__dirname, '/index.html'));
-// });
+// Serve static files from the React app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname)));
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
+}
 
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Test successful' });
@@ -37,7 +42,7 @@ io.on('connection', (socket) => {
 
   socket.on('playSound', (sample) => {
     console.log('Received playSound event');
-    socket.broadcast.emit('playSound', sample); 
+    socket.broadcast.emit('playSound', sample);
   });
 
   socket.on('disconnect', () => {
